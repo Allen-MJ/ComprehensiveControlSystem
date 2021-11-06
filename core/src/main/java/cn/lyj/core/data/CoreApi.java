efaa58d2851bbd5cf9f0b24d7779847717eb6f5c
Allen <maojing644963131@126.com>
2021-11-06 15:04:31 +0800

    人口管理

---
+++ core/src/main/java/cn/lyj/core/data/CoreApi.java
@@ -0,0 +1,36 @@
+package cn.lyj.core.data;
+
+public class CoreApi {
+    /**
+     * 户籍人口查询
+     */
+    public static final String _core_1 = "/api/tb12/appQuery";
+    /**
+     * 户籍人口添加
+     */
+    public static final String _core_2 = "/api/tb12/appAdd";
+    /**
+     * 户籍人口修改
+     */
+    public static final String _core_3 = "/api/tb12/appUpdate";
+    /**
+     * 户籍人口删除
+     */
+    public static final String _core_4 = "/api/tb12/appDel";
+    /**
+     * 流动人口添加
+     */
+    public static final String _core_5 = "/api/tb13/appAdd";
+    /**
+     * 流动人口删除
+     */
+    public static final String _core_6 = "/api/tb13/appDel";
+    /**
+     * 流动人口修改
+     */
+    public static final String _core_7 = "/api/tb13/appUpdate";
+    /**
+     * 流动人口查询
+     */
+    public static final String _core_5 = "/api/tb13/appQuery";
+}
---
+++ core/src/main/java/cn/lyj/core/person/PersonListActivity.java
@@ -0,0 +1,10 @@
+package cn.lyj.core.person;
+
+import allen.frame.AllenBaseActivity;
+
+/**
+ * 实有人口管理列表
+ */
+public class PersonListActivity extends AllenBaseActivity {
+
+}
